7365386e7b1d20de47822e7243d773f2c8540b13
3rag0n <3rag0n@protonmail.com>
2020-09-30 22:51:16 +0000

    token removed

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.login('NzU5ODkyOTM0MzgzODk0NTM3.X3EHcQ.b96--zoDgrdnbWwAy_9mRn852E8');
-
 client.on('message', message => {
 	if (message.content === '!ping') {
 	// send back "Pong." to the channel the message was sent in
