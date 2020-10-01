f86233735a89134fc58885d51dddb5f690bb15dc
3rag0n <3rag0n@protonmail.com>
2020-10-01 03:20:16 +0000

    giphy bot

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
+const { prefix, token, giphyToken } = require('./config.json'); 
 const client = new Discord.Client();
+
+var GphApiClient = require('giphy-js-sdk-core')
+giphy = GphApiClient(giphyToken)
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('message', message => {
-	if (message.content === '!ping') {
-	// send back "Pong." to the channel the message was sent in
-	message.channel.send('Pong.');
-}
-});
+client.on('message', (message) => {
+	if (message.author.bot) return;
+	if (message.content == "theboiz"){
+		giphy.random('gifs', {'q':"The Boys"})
+			.then((response) => {
+				//message.channel.send("WORK");
+				console.log(response);
+				message.channel.send("BOIZ",{
+					files: [response.images.fixed_height.url]
+			});
 
+			
+			//var totalResponses = response.data.length; 
+			//var responseIndex = Math.floor((Math.random() * 10) + 1) % totalResponses; 
+			//var responseFinal = response.data[responseIndex]; 
+			
+			//message.channel.send("BOIZ", {
+			//	files: [responseFinal.images.fixed_height.url]
+		}).catch(() => {
+			message.channel.send("fail");
+		})
+	}else message.channel.send("error");
+
+})
+client.login(token);
 
